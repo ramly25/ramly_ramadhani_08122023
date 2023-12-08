1762bf3c1f89ccc6c6a5d21e2f46731f15066179
ramly25 <ramlyramadhani@gmail.com>
2023-12-08 10:00:04 +0700

    create migration table employees

---
+++ database/migrations/2023_12_08_025533_add_table_pegawai.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id('pegawai_id');
+            $table->string('pegawai_nama');
+            $table->Integer('pegawai_umur');
+            $table->longtext('pegawai_alamat');
+            $table->string('foto');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
